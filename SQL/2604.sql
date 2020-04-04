7d1f303fa9240e3066d9bf33785691f5d33108cf
DrikaGaribalde <adrianagaribalde@gmail.com>
2020-04-04 17:28:15 -0300

    ok

--- SQL/2604.sql
+++ SQL/2604.sql
@@ -1,4 +1,2 @@
-select products.name, providers.name from products, providers, categories
-where products.id_providers = providers.id and products.id_categories = 6
-group by products.name, providers.name
-order by 1 desc
+select id, name from products
+where price <10 or price >100
