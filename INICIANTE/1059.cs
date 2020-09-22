b830ad7ea9112c290f4e09e710ed580613ebebb3
DrikaGaribalde <adrianagaribalde@gmail.com>
2020-09-22 09:37:59 -0300

    1059 - Números Pares - C#

---
+++ INICIANTE/1059.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class URI {
+    public static void Main() {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                Console.WriteLine(i);
+            }           
+        }
+        Console.ReadLine();
+    }
+}
